973513493a5e64faf9c031322cdf226ab9ed1417
bayunqi <bayunqi.817@bytedance.com>
2026-07-02 12:01:45 +0800

    Pin Gradio transitive dependencies

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(name='MyShell-OpenVoice',
             'cn2an==0.5.22',
             'jieba==0.42.1',
             'gradio==4.44.1',
+            'huggingface-hub==0.25.2',
+            'urllib3==1.26.20',
             'langid==1.1.6'
       ],
       zip_safe=False
